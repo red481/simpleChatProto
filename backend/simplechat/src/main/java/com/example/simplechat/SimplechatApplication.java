ea2f34d761db4d7886c558069ae0f3689b6e68d1
Dong Soo <rsy48@naver.com>
2023-12-06 14:11:58 +0900

    first commit

---
+++ backend/simplechat/src/main/java/com/example/simplechat/SimplechatApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplechatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplechatApplication.class, args);
+	}
+
+}
